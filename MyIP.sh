7d5fa764e7931b581d4526cc6ce90440879797ba
Jan Brauckmann <info@thestorm-mc.de>
2016-04-23 14:53:41 +0200

    Create MyIP.sh

---
+++ MyIP.sh
@@ -0,0 +1,16 @@
+#!bin/bash
+# Grep MyiP
+# by TheCarsLP & xyzbomber
+
+
+echo "Hole ext.iP"
+echo "MyIP is:"
+curl ifconfig.me/ip
+
+echo "Done!" #Fertig
+
+echo "Schreibe IP in Datei" #Ausgabe in TxT
+curl ifconfig.me/ip -O >> /home/myip.txt #Speicherort der Datei welche die IP beinhaltet
+echo "Fertig!"
+
+exit
